39347b47204903f07deb843be05e497c25bcfc68
Théo Cavignac <theo.cavignac@gmail.com>
2025-08-24 12:57:16 +0200

    fix: provide missing styles

--- download_pkgs_docs.sh
+++ download_pkgs_docs.sh
@@ -2,3 +2,13 @@
 wget -mpEk "https://pkg.odin-lang.org/"
 mkdir -p Odin.docset/Contents/Resources/Documents
 cp -R pkg.odin-lang.org/* Odin.docset/Contents/Resources/Documents/
+
+wget https://odin-lang.org/scss/custom.min.css
+wget https://odin-lang.org/css/style.css
+wget https://odin-lang.org/lib/highlight/styles/github-dark.min.css
+
+cat custom.min.css style.css github-dark.min.css >> Odin.docset/Contents/Resources/Documents/style.css
+
+wget https://odin-lang.org/logo.svg
+mv logo.svg Odin.docset/Contents/Resources/Documents/logo.svg
+sed 's;https://odin-lang.org/logo.svg;/odin/logo.svg;g' -i $(find Odin.docset/Contents/Resources/Documents -name "*.html")
