39e0955de7d418d00485c207d00ed583747f81a0
Thomas Teixeira <avanalos@gmail.com>
2026-01-29 22:43:22 +0100

    Fix download_pkgs_docs to include CSS styling

--- download_pkgs_docs.sh
+++ download_pkgs_docs.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-wget -mpEk "https://pkg.odin-lang.org/"
+
 mkdir -p Odin.docset/Contents/Resources/Documents
-cp -R pkg.odin-lang.org/* Odin.docset/Contents/Resources/Documents/
+wget                                                          \
+  --mirror                                                    \
+  --page-requisites                                           \
+  --adjust-extension                                          \
+  --convert-links                                             \
+  --no-host-directories                                       \
+  --directory-prefix=Odin.docset/Contents/Resources/Documents \
+  "https://pkg.odin-lang.org"                                 \
+  "https://odin-lang.org/scss/custom.min.css"                 \
+  "https://odin-lang.org/css/style.css"                       \
+  "https://odin-lang.org/logo.svg"
